d62c8fdb0d5b9d3319a0168dcdeb233cf7dedd4d
alaxhenry <alaxhenry0121@gmail.com>
2022-12-14 11:38:40 +0900

    Initial Commit

---
+++ ResizableHeader/ResizableHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableHeaderApp.swift
+//  ResizableHeader
+//
+//  Created by Seungchul Ha on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct ResizableHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
